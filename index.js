b0a7d3432fcafdc702e6ef5c908e45a8b04f122f
a1p4ca <sm@murye.io>
2017-09-14 23:00:31 +0900

    upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const exec = require('child-process').exec
+
+const config = require('./config.json')
+const app = express()
+
+app.get('/', (req, res) => {
+  if (req.query.passwd !== config.passwd) return res.sendStatus(400)
+  config.path.forEach((v) => {
+    exec(`cd ${v} && git pull`, (err, stdout, stderr) => {
+      if (err) console.log(err)
+      if (stdout) console.log(stdout)
+      if (stderr) console.log(stderr)
+    })
+  })
+})
+
+app.listen(3390, () => console.log('Server start!'))
